457d1247689c3c76ac474b2ab332889fcf3078ab
gtopinio <mctopinio@up.edu.ph>
2023-05-09 01:03:08 +0800

    release: initial commit

---
+++ src/main/java/com/github/gtopinio/socket_matrix_distribution/App.java
@@ -0,0 +1,13 @@
+package com.github.gtopinio.socket_matrix_distribution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
