5d06988fbc1dc87c2feb49e777cc341a3366c2c4
gtopinio <mctopinio@up.edu.ph>
2023-05-09 01:15:10 +0800

    docs: add author and project description

--- src/main/java/com/github/gtopinio/socket_matrix_distribution/App.java
+++ src/main/java/com/github/gtopinio/socket_matrix_distribution/App.java
@@ -1,7 +1,10 @@
 package com.github.gtopinio.socket_matrix_distribution;
 
 /**
- * Hello world!
+ * Author: Mark Genesis C. Topinio
+ * Creation Date: May 09, 2023 1:13 AM
+ * Project: Socket Matrix Distribution
+ * Couse: CMSC 180 - Introduction to Parallel Computing
  *
  */
 public class App 
